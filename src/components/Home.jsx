9f68eb61293a224471e8a0e454df393580dd3774
Mcampo-26 <77281924+Mcampo-26@users.noreply.github.com>
2024-10-01 14:07:47 -0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,143 +1,289 @@
-import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash } from '@fortawesome/free-solid-svg-icons'; // Importar ícono de tachito
-import Cerveza from '../assets/Cerveza.jpg';
-import Fernet from '../assets/Fernet.jpg';
-import Gin from '../assets/Gin.jpg';
-import Vodka from '../assets/Vodka.jpg';
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { usePaymentStore } from "../store/usePaymentStore";
+import { useProductStore } from "../store/useProductStore";
+import ReactQRCode from "react-qr-code";
+import Swal from "sweetalert2";
+import { useLocation, useNavigate } from "react-router-dom";
 
-export const Home = () => {
-  // Estado para manejar la cantidad de cada producto
-  const [products, setProducts] = useState([
-    { id: 1, name: 'Cerveza', image: Cerveza, quantity: 0 },
-    { id: 2, name: 'Fernet', image: Fernet, quantity: 0 },
-    { id: 3, name: 'Gin', image: Gin, quantity: 0 },
-    { id: 4, name: 'Vodka', image: Vodka, quantity: 0 },
-  ]);
+const Home = () => {
+  const { createPaymentLink, paymentLink, paymentLoading } = usePaymentStore();
+  const { products, fetchProducts, needsUpdate, setNeedsUpdate } =
+    useProductStore();
+  const [showQR, setShowQR] = useState(false);
+  const [localProducts, setLocalProducts] = useState([]); // Para gestionar cantidades de productos seleccionados
+  const location = useLocation(); // Para obtener los parámetros de la URL
+  const navigate = useNavigate(); // Para redirigir después de los pagos
 
-  // Función para incrementar la cantidad
+  // Obtener productos al cargar el componente
+  useEffect(() => {
+    fetchProducts(); // Se obtienen los productos cuando se monta el componente
+  }, [fetchProducts]);
+
+  // Actualizar productos cuando detectamos que hay una actualización pendiente
+  useEffect(() => {
+    if (needsUpdate) {
+      fetchProducts(); // Volver a obtener los productos si hay cambios
+      setNeedsUpdate(false); // Restablecer la bandera después de la actualización
+    }
+  }, [needsUpdate, fetchProducts, setNeedsUpdate]);
+
+  // Sincronizar productos locales con el estado global
+  useEffect(() => {
+    const initializedProducts = products.map((product) => ({
+      ...product,
+      quantity: 0, // Inicializa la cantidad en 0 para cada producto
+    }));
+    setLocalProducts(initializedProducts);
+  }, [products]);
+
+  // Verificar el estado del pago al cargar el componente
+  useEffect(() => {
+    const queryParams = new URLSearchParams(location.search);
+    const status = queryParams.get("status");
+  
+    // Log para verificar los parámetros de la URL que envía Mercado Pago
+    console.log('Parámetros de la URL:', queryParams.toString());
+    console.log('Estado del pago recibido:', status);
+  
+    if (status === "approved") {
+      console.log("El pago fue aprobado.");
+      Swal.fire({
+        title: "¡Pago Exitoso!",
+        text: "Gracias por tu compra.",
+        icon: "success",
+        confirmButtonText: "OK",
+      });
+      navigate("/"); // Redirige después de mostrar la alerta
+    } else if (status === "pending") {
+      console.log("El pago está pendiente.");
+      Swal.fire({
+        title: "Pago Pendiente",
+        text: "Tu pago está pendiente de confirmación.",
+        icon: "info",
+        confirmButtonText: "OK",
+      });
+      navigate("/"); // Redirige después de mostrar la alerta
+    } else if (status === "failure") {
+      console.log("El pago fue rechazado.");
+      Swal.fire({
+        title: "Pago Rechazado",
+        text: "Tu pago no pudo ser procesado.",
+        icon: "error",
+        confirmButtonText: "OK",
+      });
+      navigate("/"); // Redirige después de mostrar la alerta
+    }
+  }, [location, navigate]);
+  
+
   const incrementQuantity = (id) => {
-    setProducts(products.map(product =>
-      product.id === id ? { ...product, quantity: product.quantity + 1 } : product
-    ));
+    setLocalProducts(
+      localProducts.map((product) =>
+        product._id === id
+          ? { ...product, quantity: product.quantity + 1 }
+          : product
+      )
+    );
   };
 
-  // Función para decrementar la cantidad
   const decrementQuantity = (id) => {
-    setProducts(products.map(product =>
-      product.id === id && product.quantity > 0
-        ? { ...product, quantity: product.quantity - 1 } : product
-    ));
+    setLocalProducts(
+      localProducts.map((product) =>
+        product._id === id && product.quantity > 0
+          ? { ...product, quantity: product.quantity - 1 }
+          : product
+      )
+    );
   };
 
-  // Función para eliminar un producto del listado seleccionado
   const removeProduct = (id) => {
-    setProducts(products.map(product =>
-      product.id === id ? { ...product, quantity: 0 } : product
-    ));
+    setLocalProducts(
+      localProducts.map((product) =>
+        product._id === id ? { ...product, quantity: 0 } : product
+      )
+    );
   };
 
-  // Filtrar los productos que tienen una cantidad mayor que 0
-  const selectedProducts = products.filter(product => product.quantity > 0);
+  // Función para poner en cero todos los productos
+  const resetProducts = () => {
+    setLocalProducts(
+      localProducts.map((product) => ({
+        ...product,
+        quantity: 0,
+      }))
+    );
+  };
 
-  // Calcular el total de productos
-  const totalProducts = selectedProducts.reduce((total, product) => total + product.quantity, 0);
+  const selectedProducts = localProducts.filter(
+    (product) => product.quantity > 0
+  );
 
-  // Función para manejar singular y plural de "unidad"
+  const totalAmount = selectedProducts.reduce(
+    (total, product) => total + product.quantity * product.price,
+    0
+  );
+
+  const totalProducts = selectedProducts.reduce(
+    (total, product) => total + product.quantity,
+    0
+  );
+
   const formatUnits = (quantity) => {
     return quantity === 1 ? "unidad" : "unidades";
   };
 
+  const handlePayment = async () => {
+    const productName = "La Previa";
+    try {
+      await createPaymentLink(productName, totalAmount);
+      setShowQR(true);
+    } catch (error) {
+      console.error("Error al generar el enlace de pago:", error);
+    }
+  };
+
+  // Función para cerrar el modal del QR y poner en cero los productos
+  const handleCloseQR = () => {
+    setShowQR(false);
+    resetProducts(); // Poner en cero todos los productos
+  };
+
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center py-10">
-      <h1 className="text-4xl font-bold text-blue-600 mb-8">Hola, soy The Point</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-        {products.map(product => (
-          <div
-            key={product.id}
-            className="bg-white shadow-md rounded-lg p-4 flex flex-col items-center transform transition-transform duration-300 hover:scale-105 hover:shadow-lg"
-          >
-            <img
-              src={product.image}
-              alt={product.name}
-              className="w-40 h-40 object-cover mb-4"
-            />
-            <h2 className="text-xl font-semibold">{product.name}</h2>
-            <div className="flex items-center mt-4 space-x-4">
-              <button
-                className="bg-red-500 text-white px-4 py-2 rounded"
-                onClick={() => decrementQuantity(product.id)}
-              >
-                -
-              </button>
-              <span className="text-xl font-bold">{product.quantity}</span>
-              <button
-                className="bg-green-500 text-white px-4 py-2 rounded"
-                onClick={() => incrementQuantity(product.id)}
-              >
-                +
-              </button>
-            </div>
-          </div>
-        ))}
+    <div className="relative min-h-screen bg-gray-100 flex flex-col items-center py-10 bg-gray-300">
+      {/* Si se muestra el QR, difumina el contenido detrás */}
+      <div className="mb-8 w-full max-w-5xl mx-auto px-4 lg:px-0">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-gray-400 py-3 px-6 rounded-lg inline-block">
+          Productos
+        </h1>
       </div>
 
-      {/* Listado dinámico de productos seleccionados con botón de eliminar */}
-      <div className="mt-10 w-full px-4">
-        <h2 className="text-2xl font-semibold mb-4 text-center text-gray-800">Productos seleccionados:</h2>
-        {selectedProducts.length > 0 ? (
-          <ul className="bg-white shadow-md rounded-lg p-4 divide-y divide-gray-200">
-            {selectedProducts.map(product => (
-              <li key={product.id} className="flex justify-between items-center py-4">
-                <div className="flex flex-col sm:flex-row items-center space-x-4">
-                  <span className="bg-gray-100 text-gray-800 px-3 py-1 rounded-full text-sm">
-                    {product.quantity} {formatUnits(product.quantity)}
-                  </span>
-                  <span className="font-medium text-gray-700">{product.name}</span>
-                </div>
+      <div
+        className={`flex flex-col lg:flex-row w-full ${
+          showQR ? "blur-md" : ""
+        }`}
+      >
+        <div className="flex-1 grid grid-cols-1 gap-8 px-4 md:px-8 mt-20">
+          {localProducts.map((product) => (
+            <div
+              key={product._id}
+              className="bg-white shadow-md rounded-lg p-4 flex items-center justify-between transform transition-transform duration-300 hover:scale-105 hover:shadow-lg"
+            >
+              <div className="flex items-center">
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  className="w-16 h-16 md:w-24 md:h-24 object-cover mr-4"
+                />
+                <h2 className="text-lg md:text-xl font-semibold">
+                  {product.name} - ${product.price}
+                </h2>
+              </div>
+              <div className="flex items-center space-x-2 md:space-x-4">
                 <button
-                  className="text-red-500 hover:text-red-700 font-medium"
-                  onClick={() => removeProduct(product.id)}
+                  className="bg-red-500 text-white px-2 md:px-4 py-1 md:py-2 rounded"
+                  onClick={() => decrementQuantity(product._id)}
                 >
-                  <FontAwesomeIcon icon={faTrash} />
+                  -
                 </button>
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p className="text-gray-500 text-center">No has seleccionado ningún producto.</p>
-        )}
+                <span className="text-lg md:text-xl font-bold">
+                  {product.quantity}
+                </span>
+                <button
+                  className="bg-green-500 text-white px-2 md:px-4 py-1 md:py-2 rounded"
+                  onClick={() => incrementQuantity(product._id)}
+                >
+                  +
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="w-full lg:w-1/3 mt-10 lg:mt-0 lg:ml-8 lg:sticky lg:top-4">
+          <h2 className="text-xl md:text-2xl font-semibold mb-4 text-center text-gray-800">
+            Resumen de compra
+          </h2>
+          {selectedProducts.length > 0 ? (
+            <div className="bg-white shadow-md rounded-lg p-4">
+              <ul className="divide-y divide-gray-200">
+                {selectedProducts.map((product) => (
+                  <li
+                    key={product._id}
+                    className="flex justify-between items-center py-4"
+                  >
+                    <div className="flex flex-col sm:flex-row items-center space-x-2 md:space-x-4">
+                      <span className="bg-gray-100 text-gray-800 px-3 py-1 rounded-full text-sm">
+                        {product.quantity} {formatUnits(product.quantity)}
+                      </span>
+                      <span className="font-medium text-gray-700">
+                        {product.name}
+                      </span>
+                    </div>
+                    <button
+                      className="text-red-500 hover:text-red-700 font-medium"
+                      onClick={() => removeProduct(product._id)}
+                    >
+                      <FontAwesomeIcon icon={faTrash} />
+                    </button>
+                  </li>
+                ))}
+              </ul>
+
+              <div className="mt-4 border-t pt-4 flex justify-between font-bold">
+                <span>Total de productos:</span>
+                <span>
+                  {totalProducts} {formatUnits(totalProducts)}
+                </span>
+              </div>
+              <div className="mt-4 border-t pt-4 flex justify-between font-bold">
+                <span>Total a pagar:</span>
+                <span>${totalAmount}</span>
+              </div>
+            </div>
+          ) : (
+            <p className="text-gray-500 text-center">
+              No has seleccionado ningún producto.
+            </p>
+          )}
+
+          {selectedProducts.length > 0 && (
+            <div className="mt-6">
+              <button
+                className="bg-blue-600 text-white px-4 md:px-6 py-2 md:py-3 rounded-lg shadow-lg hover:bg-blue-700 transition duration-300 w-full"
+                onClick={handlePayment}
+              >
+                {paymentLoading
+                  ? "Generando enlace..."
+                  : `Comprar por $${totalAmount}`}
+              </button>
+            </div>
+          )}
+        </div>
       </div>
 
-      {/* Resumen de productos (Ticket) */}
-      {selectedProducts.length > 0 && (
-        <div className="mt-6 w-full px-4 max-w-md">
-          <h3 className="text-xl font-bold mb-4 text-gray-800">Resumen de compra </h3>
-          <div className="bg-white shadow-md rounded-lg p-4">
-            <ul className="space-y-2">
-              {selectedProducts.map(product => (
-                <li key={product.id} className="flex justify-between">
-                  <span>{product.name}</span>
-                  <span>{product.quantity} {formatUnits(product.quantity)}</span>
-                </li>
-              ))}
-            </ul>
-            <div className="mt-4 border-t pt-4 flex justify-between font-bold">
-              <span>Total de productos:</span>
-              <span>{totalProducts} {formatUnits(totalProducts)}</span>
+      {showQR && paymentLink && (
+        <div className="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center z-50">
+          <div className="relative bg-white p-6 rounded-lg shadow-lg w-11/12 sm:w-4/5 max-w-md h-auto">
+            <button
+              className="absolute -top-4 -right-4 text-red-500 hover:text-red-700 bg-white rounded-full p-2"
+              onClick={handleCloseQR} // Llamar a handleCloseQR para cerrar el modal y resetear los productos
+            >
+              <FontAwesomeIcon icon={faTimes} size="xl" className="text-red-500 cursor-pointer transition-transform duration-200 hover:rotate-90" />
+            </button>
+            <div className="flex justify-center items-center w-full">
+              <ReactQRCode
+                value={paymentLink}
+                size={450}
+                className="max-w-full h-auto"
+              />
             </div>
           </div>
-        </div>
-      )}
-
-      {/* Botón de Comprar */}
-      {selectedProducts.length > 0 && (
-        <div className="mt-6">
-          <button className="bg-blue-600 text-white px-6 py-3 rounded-lg shadow-lg hover:bg-blue-700 transition duration-300">
-            Comprar
-          </button>
         </div>
       )}
     </div>
   );
 };
+
+export default Home;
