7c8eec7ea5f9768bc5eb22cef2d6b23de4876f0c
Mcampo-26 <77281924+Mcampo-26@users.noreply.github.com>
2024-10-04 16:45:18 -0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,32 +3,197 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { usePaymentStore } from "../store/usePaymentStore";
 import { useProductStore } from "../store/useProductStore";
+import ReactQRCode from "react-qr-code";
 import Swal from "sweetalert2";
+import io from "socket.io-client";
+
+// URL de tu servidor WebSocket en Heroku (asegúrate de que el backend esté correctamente configurado)
+const socket = io("https://thepointback-03939a97aeeb.herokuapp.com", {
+  transports: ["websocket"],
+  reconnectionAttempts: 5, // Número de intentos de reconexión
+  reconnectionDelay: 3000, // Retraso entre intentos de reconexión
+});
 
 const Home = () => {
-  const { products, fetchProducts, needsUpdate, setNeedsUpdate } = useProductStore();
-  const [localProducts, setLocalProducts] = useState([]);
+  const { createPaymentLink, paymentLink, paymentLoading } = usePaymentStore();
+  const { products, fetchProducts, needsUpdate, setNeedsUpdate } =
+    useProductStore();
   const [showQR, setShowQR] = useState(false);
-  const hiddenTicketRef = useRef(null); 
+  const [localProducts, setLocalProducts] = useState([]); // Para gestionar cantidades de productos seleccionados
+  const [paymentStatus, setPaymentStatus] = useState(null); // Estado del pago
+  const [paymentId, setPaymentId] = useState(null); // ID del pago
+  const hiddenTicketRef = useRef(null); // Para acceder al contenido del ticket de forma oculta
 
+  // Obtener productos al cargar el componente
   useEffect(() => {
-    fetchProducts(); // Obtener productos
+    fetchProducts(); // Se obtienen los productos cuando se monta el componente
   }, [fetchProducts]);
 
+  // Actualizar productos cuando detectamos que hay una actualización pendiente
   useEffect(() => {
     if (needsUpdate) {
-      fetchProducts();
-      setNeedsUpdate(false);
+      fetchProducts(); // Volver a obtener los productos si hay cambios
+      setNeedsUpdate(false); // Restablecer la bandera después de la actualización
     }
   }, [needsUpdate, fetchProducts, setNeedsUpdate]);
 
+  // Sincronizar productos locales con el estado global
   useEffect(() => {
     const initializedProducts = products.map((product) => ({
       ...product,
-      quantity: 0, // Inicializamos con cantidad 0
+      quantity: 0, // Inicializa la cantidad en 0 para cada producto
     }));
     setLocalProducts(initializedProducts);
   }, [products]);
+
+  // Conectar al servidor WebSocket y escuchar eventos
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("Conectado al servidor WebSocket");
+    });
+
+    socket.on("paymentSuccess", ({ status, paymentId }) => {
+      handlePaymentResult(status, paymentId); // Usa la función para manejar el resultado del pago
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Desconectado del servidor WebSocket");
+    });
+
+    // Limpiar el listener cuando el componente se desmonta
+    return () => {
+      socket.off("paymentSuccess");
+      socket.disconnect(); // Desconectar el socket cuando el componente se desmonte
+    };
+  }, []);
+
+  // Función para manejar el estado del pago y resetear productos
+  const handlePaymentResult = (status, paymentId) => {
+    const printTickets = () => {
+      let allTicketsContent = selectedProducts
+        .flatMap((product) => {
+          return Array.from({ length: product.quantity }).map(() => {
+            return `
+              <div class="ticket-container">
+                <h2 class="ticket-title">1x</h2>
+                <p class="ticket-item">${getArticle(product.name)}</p>
+                <h2 class="ticket-footer">Gracias por tu compra.</h2>
+              </div>
+            `;
+          });
+        })
+        .join(''); // Eliminar cualquier espacio entre tickets
+    
+      const iframe = document.createElement("iframe");
+      document.body.appendChild(iframe);
+      iframe.style.position = "absolute";
+      iframe.style.width = "0px";
+      iframe.style.height = "0px";
+      const doc = iframe.contentWindow.document;
+      doc.open();
+      doc.write(`
+        <html>
+          <head>
+            <style>
+              /* Estilos generales del ticket */
+              body {
+                 margin: 0;
+                 padding: 0;
+               
+                
+              }
+    
+              /* Eliminar márgenes superiores e inferiores de los contenedores */
+              .ticket-container {
+                width: 7cm;             
+                height: 6cm; /* Asegurar una altura específica */
+                margin: 0; /* Sin margen en el contenedor */              
+                display: flex;
+                flex-direction: column;
+                justify-content: center; /* Centrar contenido verticalmente */
+                align-items: center;
+                box-sizing: border-box;
+                overflow: hidden;
+                page-break-inside: avoid; /* Evitar cortes en los tickets */
+                padding-left: 2cm; /* Ajusta este valor según lo necesites */
+              }
+    
+              /* Estilos para los elementos dentro del ticket */
+              .ticket-title, .ticket-item, .ticket-footer {
+                margin: 0;
+                padding: 0;
+                text-align: center;
+              }
+    
+              /* Eliminar márgenes y padding en el título */
+              .ticket-title {
+                font-size: 35px;
+                padding-bottom: 2px;
+              }
+    
+              /* Control preciso del tamaño de la fuente del nombre del producto */
+              .ticket-item {
+                font-size: 100px;
+                padding: 0;
+              }
+    
+              /* Control sobre el footer para evitar cualquier espacio adicional */
+              .ticket-footer {
+                font-size: 25x;
+                margin-top: 3px;
+                padding: 0;
+              }
+    
+              /* Forzar sin margen entre tickets consecutivos */
+              .ticket-container + .ticket-container {
+                margin-top: 0 !important;
+                padding-top: 0 !important;
+              }
+            </style>
+          </head>
+          <body>${allTicketsContent}</body>
+        </html>
+      `);
+      doc.close();
+      iframe.contentWindow.focus();
+      iframe.contentWindow.print();
+      document.body.removeChild(iframe);
+    };
+    
+    
+  
+    if (status === "approved") {
+      Swal.fire({
+        title: "¡Pago Exitoso!",
+        text: "Gracias por tu compra.",
+        icon: "success",
+        showConfirmButton: false,
+        timer: 1500,
+      }).then(() => {
+        setTimeout(() => {
+          printTickets();
+          window.location.reload();
+        }, 1000);
+      });
+    }
+  };
+  
+  // Función para cerrar el modal del QR y resetear productos
+  const handleCloseQR = () => {
+    setShowQR(false); // Cerrar el modal del QR
+  };
+
+  // Función que resetea todo: productos, estado del pago y QR
+  const resetAll = () => {
+    setLocalProducts((prevProducts) =>
+      prevProducts.map((product) => ({
+        ...product,
+        quantity: 0, // Resetea la cantidad de todos los productos
+      }))
+    );
+    setPaymentStatus(null); // Resetea el estado del pago
+    setPaymentId(null); // Resetea el ID de la orden
+  };
 
   const incrementQuantity = (id) => {
     setLocalProducts(
@@ -76,142 +241,30 @@ const Home = () => {
     return quantity === 1 ? "unidad" : "unidades";
   };
 
-  const handleApprovedPayment = () => {
-    const paymentResult = { status: "approved", paymentId: "fakePaymentId12345" };
-    handlePaymentResult(paymentResult.status, paymentResult.paymentId);
+  const handlePayment = async () => {
+    const productName = "La Previa";
+    try {
+      await createPaymentLink(productName, totalAmount);
+      setShowQR(true);
+    } catch (error) {
+      console.error("Error al generar el enlace de pago:", error);
+    }
   };
-
-   
- // Función para determinar si usar "un" o "una"
- const getArticle = (productName) => {
-  return `<span class="product-name">${productName}</span>`;
-};
-
-
-
-
-const handlePaymentResult = (status, paymentId) => {
-  const printTickets = () => {
-    let allTicketsContent = selectedProducts
-      .flatMap((product) => {
-        return Array.from({ length: product.quantity }).map(() => {
-          return `
-            <div class="ticket-container">
-              <h2 class="ticket-title">1x</h2>
-              <p class="ticket-item">${getArticle(product.name)}</p>
-              <h2 class="ticket-footer">Gracias por tu compra.</h2>
-            </div>
-          `;
-        });
-      })
-      .join(''); // Eliminar cualquier espacio entre tickets
-  
-    const iframe = document.createElement("iframe");
-    document.body.appendChild(iframe);
-    iframe.style.position = "absolute";
-    iframe.style.width = "0px";
-    iframe.style.height = "0px";
-    const doc = iframe.contentWindow.document;
-    doc.open();
-    doc.write(`
-      <html>
-        <head>
-          <style>
-            /* Estilos generales del ticket */
-            body {
-               margin: 0;
-               padding: 0;
-             
-              
-            }
-  
-            /* Eliminar márgenes superiores e inferiores de los contenedores */
-            .ticket-container {
-              width: 10cm;             
-              height: 6cm; /* Asegurar una altura específica */
-              margin: 0; /* Sin margen en el contenedor */              
-              display: flex;
-              flex-direction: column;
-              justify-content: center; /* Centrar contenido verticalmente */
-              align-items: center;
-              box-sizing: border-box;
-              overflow: hidden;
-              page-break-inside: avoid; /* Evitar cortes en los tickets */
-              padding-left: 2cm; /* Ajusta este valor según lo necesites */
-            }
-  
-            /* Estilos para los elementos dentro del ticket */
-            .ticket-title, .ticket-item, .ticket-footer {
-              margin: 0;
-              padding: 0;
-              text-align: center;
-            }
-  
-            /* Eliminar márgenes y padding en el título */
-            .ticket-title {
-              font-size: 35px;
-              padding-bottom: 2px;
-            }
-  
-            /* Control preciso del tamaño de la fuente del nombre del producto */
-            .ticket-item {
-              font-size: 100px;
-              padding: 0;
-            }
-  
-            /* Control sobre el footer para evitar cualquier espacio adicional */
-            .ticket-footer {
-              font-size: 18px;
-              margin-top: 3px;
-              padding: 0;
-            }
-  
-            /* Forzar sin margen entre tickets consecutivos */
-            .ticket-container + .ticket-container {
-              margin-top: 0 !important;
-              padding-top: 0 !important;
-            }
-          </style>
-        </head>
-        <body>${allTicketsContent}</body>
-      </html>
-    `);
-    doc.close();
-    iframe.contentWindow.focus();
-    iframe.contentWindow.print();
-    document.body.removeChild(iframe);
-  };
-  
-  
-
-  if (status === "approved") {
-    Swal.fire({
-      title: "¡Pago Exitoso!",
-      text: "Gracias por tu compra.",
-      icon: "success",
-      showConfirmButton: false,
-      timer: 1500,
-    }).then(() => {
-      setTimeout(() => {
-        printTickets();
-        window.location.reload();
-      }, 1000);
-    });
-  }
-};
-
-  
-  
 
   return (
     <div className="relative min-h-screen bg-gray-100 flex flex-col items-center py-10 bg-gray-300">
+      {/* Si se muestra el QR, difumina el contenido detrás */}
       <div className="mb-8 w-full max-w-5xl mx-auto px-4 lg:px-0">
         <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-gray-400 py-3 px-6 rounded-lg inline-block">
           Productos
         </h1>
       </div>
 
-      <div className="flex flex-col lg:flex-row w-full">
+      <div
+        className={`flex flex-col lg:flex-row w-full ${
+          showQR ? "blur-md" : ""
+        }`}
+      >
         <div className="flex-1 grid grid-cols-1 gap-8 px-4 md:px-8 mt-20">
           {localProducts.map((product) => (
             <div
@@ -285,7 +338,7 @@ const handlePaymentResult = (status, paymentId) => {
                   {totalProducts} {formatUnits(totalProducts)}
                 </span>
               </div>
-              <div className="mt-4 border-t pt-4 flex justify-between font-bold">
+              <div className="mt-4 border-t pt-4 flex justify_between font-bold">
                 <span>Total a pagar:</span>
                 <span>${totalAmount}</span>
               </div>
@@ -299,15 +352,41 @@ const handlePaymentResult = (status, paymentId) => {
           {selectedProducts.length > 0 && (
             <div className="mt-6">
               <button
-                className="bg-green-600 text-white px-4 md:px-6 py-2 md:py-3 rounded-lg shadow-lg hover:bg-green-700 transition duration-300 w-full"
-                onClick={handleApprovedPayment} // Botón para simular pago aprobado
+                className="bg-blue-600 text-white px-4 md:px-6 py-2 md:py-3 rounded-lg shadow-lg hover:bg-blue-700 transition duration-300 w-full"
+                onClick={handlePayment}
               >
-                Pago aprobado
+                {paymentLoading
+                  ? "Generando enlace..."
+                  : `Comprar por $${totalAmount}`}
               </button>
             </div>
           )}
         </div>
       </div>
+
+      {showQR && paymentLink && (
+        <div className="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center z-50">
+          <div className="relative bg-white p-6 rounded-lg shadow-lg w-11/12 sm:w-4/5 max-w-md h-auto">
+            <button
+              className="absolute -top-4 -right-4 text-red-500 hover:text-red-700 bg-white rounded-full p-2"
+              onClick={handleCloseQR} // Llamar a handleCloseQR para cerrar el modal y resetear los productos
+            >
+              <FontAwesomeIcon
+                icon={faTimes}
+                size="xl"
+                className="text-red-500 cursor-pointer transition-transform duration-200 hover:rotate-90"
+              />
+            </button>
+            <div className="flex justify-center items-center w-full">
+              <ReactQRCode
+                value={paymentLink}
+                size={450}
+                className="max-w-full h-auto"
+              />
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
