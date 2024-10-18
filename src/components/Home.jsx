861500d433a0fbaa0865d8dc418b2ad13590fc99
Mcampo-26 <77281924+Mcampo-26@users.noreply.github.com>
2024-10-17 22:09:53 -0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,29 +1,27 @@
 import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash, faTimes } from "@fortawesome/free-solid-svg-icons";
-import { usePaymentStore } from "../store/usePaymentStore";
+import { usePaymentStore } from "../store/usePaymentStore"; // Asegúrate de que usePaymentStore esté correctamente importado
 import { useProductStore } from "../store/useProductStore";
-import ReactQRCode from "react-qr-code";
 import Swal from "sweetalert2";
 import io from "socket.io-client";
 
 // URL de tu servidor WebSocket en Heroku
 const socket = io("https://thepointback-03939a97aeeb.herokuapp.com", {
   transports: ["websocket"],
-  reconnectionAttempts: 5, // Número de intentos de reconexión
-  reconnectionDelay: 3000, // Retraso entre intentos de reconexión
+  reconnectionAttempts: 5,
+  reconnectionDelay: 3000,
 });
 
 const Home = () => {
-  const { createPaymentLink, paymentLink, paymentLoading } = usePaymentStore();
-  const { products, fetchProducts, needsUpdate, setNeedsUpdate } =
-    useProductStore();
+  // Cambia createPaymentLink a createDynamicQR
+  const { createDynamicQR, paymentLink, qrCodeURL, paymentLoading } = usePaymentStore();
+  const { products, fetchProducts, needsUpdate, setNeedsUpdate } = useProductStore();
   const [localProducts, setLocalProducts] = useState([]); // Para gestionar cantidades de productos seleccionados
   const [showQR, setShowQR] = useState(false); // Estado para mostrar/ocultar el QR
   const [paymentStatus, setPaymentStatus] = useState(null); // Estado del pago
   const [paymentId, setPaymentId] = useState(null); // ID del pago
 
-  // Obtener productos al cargar el componente
   useEffect(() => {
     fetchProducts();
   }, [fetchProducts]);
@@ -35,7 +33,6 @@ const Home = () => {
     }
   }, [needsUpdate, fetchProducts, setNeedsUpdate]);
 
-  // Inicializa los productos en el estado con cantidad 0
   useEffect(() => {
     const initializedProducts = products.map((product) => ({
       ...product,
@@ -44,12 +41,10 @@ const Home = () => {
     setLocalProducts(initializedProducts);
   }, [products]);
 
-  // Almacenar productos seleccionados en localStorage
   useEffect(() => {
     localStorage.setItem("selectedProducts", JSON.stringify(localProducts));
   }, [localProducts]);
 
-  // Maneja el evento WebSocket para recibir el pago exitoso
   useEffect(() => {
     socket.on("connect", () => {
       console.log("Conectado al servidor WebSocket");
@@ -69,7 +64,7 @@ const Home = () => {
     };
   }, []);
 
-  // Maneja el resultado del pago
+  // Función para manejar el resultado del pago
   const handlePaymentResult = async (status, paymentId) => {
     const storedProducts = JSON.parse(localStorage.getItem("selectedProducts")) || [];
     const selectedProducts = storedProducts.filter((product) => product.quantity > 0);
@@ -88,7 +83,7 @@ const Home = () => {
       handleCloseQR();
       setTimeout(() => {
         printTickets(selectedProducts);
-         window.location.reload();
+        window.location.reload();
       }, 100);
     } else if (status === "rejected") {
       await Swal.fire({
@@ -101,13 +96,7 @@ const Home = () => {
     }
   };
 
-  // Función para imprimir los tickets
   const printTickets = (selectedProducts) => {
-    if (selectedProducts.length === 0) {
-      console.log("No hay productos seleccionados para imprimir.");
-      return;
-    }
-
     let ticketContent = selectedProducts
       .map((product) =>
         Array.from({ length: product.quantity })
@@ -116,7 +105,7 @@ const Home = () => {
               <div class="ticket-container">
                 <h2 class="ticket-title">1x</h2>
                 <p class="ticket-item">${product.name}</p>
-                <h2 class="ticket-footer">gracias por su compra...</h2>
+                <h2 class="ticket-footer">Precio: ${product.price}</h2>
               </div>`
           )
           .join("")
@@ -148,19 +137,28 @@ const Home = () => {
     printWindow.document.close();
   };
 
-  // Función para cerrar el QR
   const handleCloseQR = () => {
     setShowQR(false);
   };
 
-  // Simula un pago exitoso para pruebas
-  const simulatePaymentSuccess = () => {
-    const fakePaymentId = "12345"; // ID de pago simulado
-    const fakeStatus = "approved"; // Estado simulado
-    handlePaymentResult(fakeStatus, fakePaymentId); // Llama a la función que maneja el resultado del pago
+  // Cambia el nombre de la función para llamar a createDynamicQR
+  const handlePayment = async () => {
+    const productName = "La Previa";
+    const socketId = socket.id; // Obtener el ID del socket conectado
+    const selectedProducts = localProducts.filter((product) => product.quantity > 0);
+    const totalAmount = selectedProducts.reduce(
+      (total, product) => total + product.quantity * product.price,
+      0
+    );
+  
+    try {
+      await createDynamicQR(productName, totalAmount, selectedProducts, socketId); // Usar createDynamicQR en lugar de createPaymentLink
+      setShowQR(true);
+    } catch (error) {
+      console.error("Error al generar el QR dinámico:", error);
+    }
   };
 
-  // Incrementa la cantidad de productos
   const incrementQuantity = (id) => {
     setLocalProducts(
       localProducts.map((product) =>
@@ -171,7 +169,6 @@ const Home = () => {
     );
   };
 
-  // Decrementa la cantidad de productos
   const decrementQuantity = (id) => {
     setLocalProducts(
       localProducts.map((product) =>
@@ -182,7 +179,6 @@ const Home = () => {
     );
   };
 
-  // Elimina un producto del carrito
   const removeProduct = (id) => {
     setLocalProducts(
       localProducts.map((product) =>
@@ -191,9 +187,11 @@ const Home = () => {
     );
   };
 
-  const selectedProducts = localProducts.filter(
-    (product) => product.quantity > 0
-  );
+  const formatUnits = (quantity) => {
+    return quantity === 1 ? "unidad" : "unidades";
+  };
+
+  const selectedProducts = localProducts.filter((product) => product.quantity > 0);
 
   const totalAmount = selectedProducts.reduce(
     (total, product) => total + product.quantity * product.price,
@@ -205,38 +203,20 @@ const Home = () => {
     0
   );
 
-  const formatUnits = (quantity) => {
-    return quantity === 1 ? "unidad" : "unidades";
-  };
-
-  const handlePayment = async () => {
-    const productName = "La Previa";
-    try {
-      await createPaymentLink(productName, totalAmount);
-      setShowQR(true);
-    } catch (error) {
-      console.error("Error al generar el enlace de pago:", error);
-    }
-  };
-
   return (
-    <div className="relative min-h-screen bg-gray-100 flex flex-col items-center py-10 bg-gray-300">
-      <div className="mb-8 w-full max-w-5xl mx-auto px-4 lg:px-0">
-        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-gray-400 py-3 px-6 rounded-lg inline-block">
+    <div className="relative min-h-screen bg-gray-100 flex flex-col items-center py-8 bg-gray-300">
+      <div className="flex justify-end">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-gray-400 rounded-lg mb-0">
           Productos
         </h1>
       </div>
 
-      <div
-        className={`flex flex-col lg:flex-row w-full ${
-          showQR ? "blur-md" : ""
-        }`}
-      >
-        <div className="flex-1 grid grid-cols-1 gap-8 px-4 md:px-8 mt-20">
+      <div className={`flex flex-col lg:flex-row w-full -mt-10 ${showQR ? "blur-md" : ""}`}>
+        <div className="flex-1 grid grid-cols-1 gap-6 px-4 md:px-8 mt-20">
           {localProducts.map((product) => (
             <div
               key={product._id}
-              className="bg-white shadow-md rounded-lg p-4 flex items-center justify-between transform transition-transform duration-300 hover:scale-105 hover:shadow-lg"
+              className="bg-white shadow-md rounded-lg p-4 flex items-center justify-between transform transition-transform duration-500 hover:scale-105 hover:shadow-lg"
             >
               <div className="flex items-center">
                 <img
@@ -255,9 +235,7 @@ const Home = () => {
                 >
                   -
                 </button>
-                <span className="text-lg md:text-xl font-bold">
-                  {product.quantity}
-                </span>
+                <span className="text-lg md:text-xl font-bold">{product.quantity}</span>
                 <button
                   className="bg-green-500 text-white px-2 md:px-4 py-1 md:py-2 rounded"
                   onClick={() => incrementQuantity(product._id)}
@@ -277,17 +255,12 @@ const Home = () => {
             <div className="bg-white shadow-md rounded-lg p-4">
               <ul className="divide-y divide-gray-200">
                 {selectedProducts.map((product) => (
-                  <li
-                    key={product._id}
-                    className="flex justify-between items-center py-4"
-                  >
+                  <li key={product._id} className="flex justify-between items-center py-4">
                     <div className="flex flex-col sm:flex-row items-center space-x-2 md:space-x-4">
                       <span className="bg-gray-100 text-gray-800 px-3 py-1 rounded-full text-sm">
                         {product.quantity} {formatUnits(product.quantity)}
                       </span>
-                      <span className="font-medium text-gray-700">
-                        {product.name}
-                      </span>
+                      <span className="font-medium text-gray-700">{product.name}</span>
                     </div>
                     <button
                       className="text-red-500 hover:text-red-700 font-medium"
@@ -311,9 +284,7 @@ const Home = () => {
               </div>
             </div>
           ) : (
-            <p className="text-gray-500 text-center">
-              No has seleccionado ningún producto.
-            </p>
+            <p className="text-gray-500 text-center">No has seleccionado ningún producto.</p>
           )}
 
           {selectedProducts.length > 0 && (
@@ -322,41 +293,25 @@ const Home = () => {
                 className="bg-blue-600 text-white px-4 md:px-6 py-2 md:py-3 rounded-lg shadow-lg hover:bg-blue-700 transition duration-300 w-full"
                 onClick={handlePayment}
               >
-                {paymentLoading
-                  ? "Generando enlace..."
-                  : `Comprar por $${totalAmount}`}
+                {paymentLoading ? "Generando enlace..." : `Comprar por $${totalAmount}`}
               </button>
             </div>
           )}
-           {/* 
-          <button
-            className="bg-yellow-500 text-white px-4 py-2 mt-4 rounded-lg shadow-lg"
-            onClick={simulatePaymentSuccess}
-          >
-            Simular Pago Exitoso
-          </button>*/}
         </div>
       </div>
 
-      {showQR && paymentLink && (
+      {showQR && qrCodeURL && (
         <div className="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center z-50">
-          <div className="relative bg-white p-6 rounded-lg shadow-lg w-11/12 sm:w-4/5 max-w-md h-auto">
+          <div className="relative bg-white p-8 rounded-lg shadow-lg w-11/12 sm:w-4/5 max-w-lg h-auto">
             <button
               className="absolute -top-4 -right-4 text-red-500 hover:text-red-700 bg-white rounded-full p-2"
               onClick={handleCloseQR}
             >
-              <FontAwesomeIcon
-                icon={faTimes}
-                size="xl"
-                className="text-red-500 cursor-pointer transition-transform duration-200 hover:rotate-90"
-              />
+              <FontAwesomeIcon icon={faTimes} size="xl" />
             </button>
-            <div className="flex justify-center items-center w-full">
-              <ReactQRCode
-                value={paymentLink}
-                size={450}
-                className="max-w-full h-auto"
-              />
+
+            <div className="flex justify-center items-center">
+              <img src={qrCodeURL} alt="Código QR para pago" className="max-w-full h-auto" />
             </div>
           </div>
         </div>
