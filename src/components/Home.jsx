1b1ec796aa861a3f52ebceda8c128be04a41ef42
Mauricio <mcampo26@gmail.com>
2024-10-19 16:28:57 -0300

    agrego imagens de botones mercado pago y modo 25

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -304,31 +304,31 @@ export const Home = () => {
             </button>
 
             <div className="flex justify-center space-x-8 mb-6">
-  <button
-    className={`flex justify-center items-center w-32 h-16 rounded-md shadow-md border border-gray-300 transition-shadow duration-200 ${
-      selectedPaymentMethod === "mercadoPago"
-        ? "shadow-lg border-blue-600 bg-gray-100"
-        : "hover:shadow-lg hover:border-blue-400"
-    } mr-16`}
-    onClick={() => setSelectedPaymentMethod("mercadoPago")}
-  >
-    <img
-      src={mercadopagoLogo}
-      alt="Mercado Pago"
-      className="w-32 h-8"
-    />
-  </button>
-  <button
-    className={`flex justify-center items-center w-32 h-16 rounded-md shadow-md border border-gray-300 transition-shadow duration-200 ${
-      selectedPaymentMethod === "modo"
-        ? "shadow-lg border-blue-500 bg-gray-100"
-        : "hover:shadow-lg hover:border-blue-300"
-    } ml-16`}
-    onClick={() => setSelectedPaymentMethod("modo")}
-  >
-    <img src={modoLogo} alt="MODO" className="w-24 h-6" />
-  </button>
-</div>
+              <button
+                className={`flex justify-center items-center w-32 h-16 rounded-md shadow-md border border-gray-300 transition-shadow duration-200 ${
+                  selectedPaymentMethod === "mercadoPago"
+                    ? "shadow-lg border-blue-600 bg-gray-100"
+                    : "hover:shadow-lg hover:border-blue-400"
+                } mr-10`}
+                onClick={() => setSelectedPaymentMethod("mercadoPago")}
+              >
+                <img
+                  src={mercadopagoLogo}
+                  alt="Mercado Pago"
+                  className="w-32 h-8"
+                />
+              </button>
+              <button
+                className={`flex justify-center items-center w-32 h-16 rounded-md shadow-md border border-gray-300 transition-shadow duration-200 ${
+                  selectedPaymentMethod === "modo"
+                    ? "shadow-lg border-blue-500 bg-gray-100"
+                    : "hover:shadow-lg hover:border-blue-300"
+                } ml-16`}
+                onClick={() => setSelectedPaymentMethod("modo")}
+              >
+                <img src={modoLogo} alt="MODO" className="w-24 h-6" />
+              </button>
+            </div>
 
             <div className="flex justify-center items-center">
               {selectedPaymentMethod === "mercadoPago" && paymentLink && (
