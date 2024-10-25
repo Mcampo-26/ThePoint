fc966f59f6ae948deb22e9674d48872c17899db1
Mcampo-26 <77281924+Mcampo-26@users.noreply.github.com>
2024-10-25 15:15:52 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import AdminPanel from './components/AdminPanel';
-import socket from './utilities/socket.js';
 
 function App() {
-  useEffect(() => {
-    socket.connect(); // Conectar socket al cargar `App`
-
-    socket.on("connect", () => {
-      console.log("Conectado al servidor WebSocket con socket ID:", socket.id);
-    });
-
-    return () => {
-      socket.disconnect(); // Desconectar socket cuando se desmonta `App`
-    };
-  }, []);
-
   return (
     <Router>
       <Routes>
