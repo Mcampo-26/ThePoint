106294d18141cffaa1196a15d9f8f47fdbed51b4
Mcampo-26 <77281924+Mcampo-26@users.noreply.github.com>
2024-11-18 18:20:52 -0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -328,13 +328,13 @@ const Home = () => {
               </button>
             </div>
           )}
-           {/* 
+           
           <button
             className="bg-yellow-500 text-white px-4 py-2 mt-4 rounded-lg shadow-lg"
             onClick={simulatePaymentSuccess}
           >
             Simular Pago Exitoso
-          </button>*/}
+          
         </div>
       </div>
 
